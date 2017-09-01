8e572f26c06c78fa7cda842e241268030e8dc642
Aderbal Farias <aderbalfarias@hotmail.com>
2017-09-01 19:24:00 +0100

    Examples to use Dapper

---
+++ Back-end/DapperContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Example.Infrastructure.Data.DapperConfig
+{
+    public class DapperContext : IDisposable
+    {
+        private readonly string _connectionString;
+
+        public DapperContext()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["EntityContext"].ConnectionString;
+        }
+
+        public SqlConnection SqlConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+        
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+            SqlConnection().Close();
+        }
+    }
+}
---
+++ Back-end/DapperQueryExampleRepository.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+using Example.Business.DataEntities;
+using Example.Business.Interfaces.Repositories;
+using Example.Infraestrutura.CrossCutting.Support.Extensions;
+using Example.Infrastructure.Data.DapperConfig;
+using Example.Infrastructure.Data.EntityConfig;
+
+namespace Example.Infrastructure.Data.Repositories
+{
+    public class LogRepository : ILogRepository, IDisposable
+    {
+        #region Fields
+        
+        private readonly EntityContext _context = new EntityContext();
+        private readonly SqlConnection _contextDapper = new DapperContext().SqlConnection();
+        private readonly Log _log = new Log();
+
+        #endregion
+
+        #region Methods
+
+        public void Add(Log entity)
+        {
+            _context.Log.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<Log> Get()
+        {
+            return _contextDapper
+                .Query<Log>("select * from Log")
+                .ToList();
+        }
+
+        public IEnumerable<Log> Get(Pagination paginar, long? processoId = null, string key = null)
+        {
+            //return _context.Log
+            //    .Include(i => i.LogType)
+            //    .Include(i => i.Usuario)
+            //    .Include(i => i.Tela)
+            //    .Skip(paginar.SkipPagina(paginar))
+            //    .Take(paginar.QtdeItensPagina)
+            //    .ToList();
+            
+            var resultList = _contextDapper.Query<Log, LogType, Usuario, Log>($@"
+                    select l.Id, l.TelaId, l.Mensagem, l.Conteudo, l.DataCadastro, l.NomeClasse,
+                        l.LogTypeId, lt.Id, lt.Descricao,
+                        l.UsuarioId, u.Id, u.Nome, u.Login
+                    from Log l
+                    inner join LogType lt on lt.Id = l.LogTypeId                
+                    left join Usuario u on u.Id = l.UsuarioId
+                    where {_log.Where(processoId, key)} 
+                    {paginar.GeneretePaginationSql(paginar, "l.Id")}", 
+                    (l, lt, u) =>
+                    {
+                        l.LogType = lt;
+                        l.Usuario = u;
+                        return l; 
+                    }, splitOn: "LogTypeId, UsuarioId")
+                .AsQueryable();
+
+            return resultList.ToList();
+        }
+
+        public int Count(long? processoId = null, string key = null)
+        {
+            return _contextDapper
+                .Query<int>($@"select Count(l.Id)
+                    from Log l
+                    inner join LogType lt on lt.Id = l.LogTypeId                
+                    left join Usuario u on u.Id = l.UsuarioId
+                    where {_log.Where(processoId, key)}")
+                .Single();
+        }
+
+        public void Dispose()
+        {
+            _contextDapper.Close();
+            _context.Dispose();
+        }
+
+        #endregion
+    }
+}
