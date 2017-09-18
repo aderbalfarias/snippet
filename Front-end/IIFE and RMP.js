d5a1ff2b4112e7ea0e8c31750581044797832716
Aderbal Farias <aderbalfarias@hotmail.com>
2017-09-18 20:04:51 +0100

    Including of file with func js

---
+++ Front-end/IIFE and RMP.js
@@ -0,0 +1,18 @@
+var example = function(){
+   var x = "I'm a example";
+
+   var whoIs = function() {
+     return x;
+   }
+
+   return {
+      itIs: whoIs
+   }
+}();
+
+example.itIs();
+
+//I can't access x because it's private in this concept
+
+// Revealing Module Pattern 
+// immediately Invoked Function Expression
