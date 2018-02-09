fbf9195b7d106d67ba3298ed262b264008d4e690
Aderbal Farias <aderbalfarias@hotmail.com>
2018-02-09 20:03:14 +0000

    Including SQL file to check stored procedures data

---
+++ SQL/Checking Stored Procedures.sql
@@ -0,0 +1,99 @@
+-- Bad Man List for stored procedures
+
+-- These are database-specific, not instance-specific
+
+-- Top Cached SPs By Execution Count (SP Execution Counts)
+SELECT TOP(100) p.name AS [SP Name], qs.execution_count,
+ISNULL(qs.execution_count/DATEDIFF(Minute, qs.cached_time, GETDATE()), 0) AS [Calls/Minute],
+qs.total_worker_time/qs.execution_count AS [AvgWorkerTime], qs.total_worker_time AS [TotalWorkerTime],  
+qs.total_elapsed_time, qs.total_elapsed_time/qs.execution_count AS [avg_elapsed_time],
+qs.cached_time
+FROM sys.procedures AS p WITH (NOLOCK)
+INNER JOIN sys.dm_exec_procedure_stats AS qs WITH (NOLOCK)
+ON p.[object_id] = qs.[object_id]
+WHERE qs.database_id = DB_ID()
+ORDER BY qs.execution_count DESC OPTION (RECOMPILE);
+
+-- Tells you which cached stored procedures are called the most often
+-- This helps you characterize and baseline your workload
+
+
+-- Top Cached SPs By Avg Elapsed Time (SP Avg Elapsed Time)
+SELECT TOP(25) p.name AS [SP Name], qs.total_elapsed_time/qs.execution_count AS [avg_elapsed_time], 
+qs.total_elapsed_time, qs.execution_count, ISNULL(qs.execution_count/DATEDIFF(Minute, qs.cached_time, 
+GETDATE()), 0) AS [Calls/Minute], qs.total_worker_time/qs.execution_count AS [AvgWorkerTime], 
+qs.total_worker_time AS [TotalWorkerTime], qs.cached_time
+FROM sys.procedures AS p WITH (NOLOCK)
+INNER JOIN sys.dm_exec_procedure_stats AS qs WITH (NOLOCK)
+ON p.[object_id] = qs.[object_id]
+WHERE qs.database_id = DB_ID()
+ORDER BY avg_elapsed_time DESC OPTION (RECOMPILE);
+
+-- This helps you find long-running cached stored procedures that
+-- may be easy to optimize with standard query tuning techniques
+
+
+-- Top Cached SPs By Total Worker time (SP Worker Time)
+SELECT TOP(25) p.name AS [SP Name], qs.total_worker_time AS [TotalWorkerTime], 
+qs.total_worker_time/qs.execution_count AS [AvgWorkerTime], qs.execution_count, 
+ISNULL(qs.execution_count/DATEDIFF(Minute, qs.cached_time, GETDATE()), 0) AS [Calls/Minute],
+qs.total_elapsed_time, qs.total_elapsed_time/qs.execution_count 
+AS [avg_elapsed_time], qs.cached_time
+FROM sys.procedures AS p WITH (NOLOCK)
+INNER JOIN sys.dm_exec_procedure_stats AS qs WITH (NOLOCK)
+ON p.[object_id] = qs.[object_id]
+WHERE qs.database_id = DB_ID()
+ORDER BY qs.total_worker_time DESC OPTION (RECOMPILE);
+
+-- This helps you find the most expensive cached stored procedures from a CPU perspective
+-- You should look at this if you see signs of CPU pressure. Worker time relates to CPU cost 
+
+
+-- Top Cached SPs By Total Logical Reads (SP Logical Reads)
+SELECT TOP(25) p.name AS [SP Name], qs.total_logical_reads AS [TotalLogicalReads], 
+qs.total_logical_reads/qs.execution_count AS [AvgLogicalReads],qs.execution_count, 
+ISNULL(qs.execution_count/DATEDIFF(Minute, qs.cached_time, GETDATE()), 0) AS [Calls/Minute], 
+qs.total_elapsed_time, qs.total_elapsed_time/qs.execution_count 
+AS [avg_elapsed_time], qs.cached_time
+FROM sys.procedures AS p WITH (NOLOCK)
+INNER JOIN sys.dm_exec_procedure_stats AS qs WITH (NOLOCK)
+ON p.[object_id] = qs.[object_id]
+WHERE qs.database_id = DB_ID()
+ORDER BY qs.total_logical_reads DESC OPTION (RECOMPILE);
+
+-- This helps you find the most expensive cached stored procedures from a memory perspective
+-- You should look at this if you see signs of memory pressure. Logical reads relate to memory pressure
+
+
+-- Top Cached SPs By Total Physical Reads (SP Physical Reads)
+SELECT TOP(25) p.name AS [SP Name],qs.total_physical_reads AS [TotalPhysicalReads], 
+qs.total_physical_reads/qs.execution_count AS [AvgPhysicalReads], qs.execution_count, 
+qs.total_logical_reads,qs.total_elapsed_time, qs.total_elapsed_time/qs.execution_count 
+AS [avg_elapsed_time], qs.cached_time 
+FROM sys.procedures AS p WITH (NOLOCK)
+INNER JOIN sys.dm_exec_procedure_stats AS qs WITH (NOLOCK)
+ON p.[object_id] = qs.[object_id]
+WHERE qs.database_id = DB_ID()
+AND qs.total_physical_reads > 0
+ORDER BY qs.total_physical_reads DESC, qs.total_logical_reads DESC OPTION (RECOMPILE);
+
+-- This helps you find the most expensive cached stored procedures from a read I/O perspective
+-- You should look at this if you see signs of I/O pressure or of memory pressure. Physical reads relate to disk I/O read pressure
+       
+
+-- Top Cached SPs By Total Logical Writes (SP Logical Writes)
+SELECT TOP(25) p.name AS [SP Name], qs.total_logical_writes AS [TotalLogicalWrites], 
+qs.total_logical_writes/qs.execution_count AS [AvgLogicalWrites], qs.execution_count,
+ISNULL(qs.execution_count/DATEDIFF(Minute, qs.cached_time, GETDATE()), 0) AS [Calls/Minute],
+qs.total_elapsed_time, qs.total_elapsed_time/qs.execution_count AS [avg_elapsed_time], 
+qs.cached_time
+FROM sys.procedures AS p WITH (NOLOCK)
+INNER JOIN sys.dm_exec_procedure_stats AS qs WITH (NOLOCK)
+ON p.[object_id] = qs.[object_id]
+WHERE qs.database_id = DB_ID()
+AND qs.total_logical_writes > 0
+ORDER BY qs.total_logical_writes DESC OPTION (RECOMPILE);
+
+-- This helps you find the most expensive cached stored procedures from a write I/O perspective
+-- You should look at this if you see signs of I/O pressure or of memory pressure
+-- Logical writes relate to both memory and disk I/O write pressure 
