6e63368213e6825a395cf7661a5f0b96df298a55
Aderbal Farias <aderbalfarias@hotmail.com>
2018-02-09 20:15:07 +0000

    Including SQL file to provide index tuning queries

---
+++ SQL/Checking Indexes.sql
@@ -0,0 +1,104 @@
+-- Index Tuning Queries
+-- Glenn Berry, SQLskills.com
+
+-- These are database-specific, not instance-specific
+
+-- Possible Bad NC Indexes (writes > reads)  (Bad NC Indexes)
+SELECT OBJECT_NAME(s.[object_id]) AS [Table Name], i.name AS [Index Name], i.index_id, 
+i.is_disabled, i.is_hypothetical, i.has_filter, i.fill_factor,
+user_updates AS [Total Writes], user_seeks + user_scans + user_lookups AS [Total Reads],
+user_updates - (user_seeks + user_scans + user_lookups) AS [Difference]
+FROM sys.dm_db_index_usage_stats AS s WITH (NOLOCK)
+INNER JOIN sys.indexes AS i WITH (NOLOCK)
+ON s.[object_id] = i.[object_id]
+AND i.index_id = s.index_id
+WHERE OBJECTPROPERTY(s.[object_id],'IsUserTable') = 1
+AND s.database_id = DB_ID()
+AND user_updates > (user_seeks + user_scans + user_lookups)
+AND i.index_id > 1
+ORDER BY [Difference] DESC, [Total Writes] DESC, [Total Reads] ASC OPTION (RECOMPILE);
+
+-- Look for indexes with high numbers of writes and zero or very low numbers of reads
+-- Consider your complete workload, and how long your instance has been running
+-- Investigate further before dropping an index!
+
+
+-- Missing Indexes for current database by Index Advantage  (Missing Indexes)
+SELECT DISTINCT CONVERT(decimal(18,2), user_seeks * avg_total_user_cost * (avg_user_impact * 0.01)) AS [index_advantage], 
+migs.last_user_seek, mid.[statement] AS [Database.Schema.Table],
+mid.equality_columns, mid.inequality_columns, mid.included_columns,
+migs.unique_compiles, migs.user_seeks, migs.avg_total_user_cost, migs.avg_user_impact,
+OBJECT_NAME(mid.[object_id]) AS [Table Name], p.rows AS [Table Rows]
+FROM sys.dm_db_missing_index_group_stats AS migs WITH (NOLOCK)
+INNER JOIN sys.dm_db_missing_index_groups AS mig WITH (NOLOCK)
+ON migs.group_handle = mig.index_group_handle
+INNER JOIN sys.dm_db_missing_index_details AS mid WITH (NOLOCK)
+ON mig.index_handle = mid.index_handle
+INNER JOIN sys.partitions AS p WITH (NOLOCK)
+ON p.[object_id] = mid.[object_id]
+WHERE mid.database_id = DB_ID() 
+ORDER BY index_advantage DESC OPTION (RECOMPILE);
+
+-- Look at index advantage, last user seek time, number of user seeks to help determine source and importance
+-- SQL Server is overly eager to add included columns, so beware
+-- Do not just blindly add indexes that show up from this query!!!
+
+
+-- Find missing index warnings for cached plans in the current database  (Missing Index Warnings)
+-- Note: This query could take some time on a busy instance
+SELECT TOP(25) OBJECT_NAME(objectid) AS [ObjectName], 
+               query_plan, cp.objtype, cp.usecounts
+FROM sys.dm_exec_cached_plans AS cp WITH (NOLOCK)
+CROSS APPLY sys.dm_exec_query_plan(cp.plan_handle) AS qp
+WHERE CAST(query_plan AS NVARCHAR(MAX)) LIKE N'%MissingIndex%'
+AND dbid = DB_ID()
+ORDER BY cp.usecounts DESC OPTION (RECOMPILE);
+
+-- Helps you connect missing indexes to specific stored procedures or queries
+-- This can help you decide whether to add them or not
+
+
+-- Look at most frequently modified indexes and statistics (Volatile Indexes)
+SELECT o.name AS [Object Name], o.[object_id], o.type_desc, s.name AS [Statistics Name], 
+       s.stats_id, s.no_recompute, s.auto_created, 
+	   sp.modification_counter, sp.rows, sp.rows_sampled, sp.last_updated
+FROM sys.objects AS o WITH (NOLOCK)
+INNER JOIN sys.stats AS s WITH (NOLOCK)
+ON s.object_id = o.object_id
+CROSS APPLY sys.dm_db_stats_properties(s.object_id, s.stats_id) AS sp
+WHERE o.type_desc NOT IN (N'SYSTEM_TABLE', N'INTERNAL_TABLE')
+AND sp.modification_counter > 0
+ORDER BY sp.modification_counter DESC, o.name OPTION (RECOMPILE);
+
+
+--- Index Read/Write stats (all tables in current DB) ordered by Reads  (Overall Index Usage - Reads)
+SELECT OBJECT_NAME(s.[object_id]) AS [ObjectName], i.name AS [IndexName], i.index_id,
+	   s.user_seeks + s.user_scans + s.user_lookups AS [Reads], s.user_updates AS [Writes],  
+	   i.type_desc AS [IndexType], i.fill_factor AS [FillFactor], i.has_filter, i.filter_definition, 
+	   s.last_user_scan, s.last_user_lookup, s.last_user_seek
+FROM sys.dm_db_index_usage_stats AS s WITH (NOLOCK)
+INNER JOIN sys.indexes AS i WITH (NOLOCK)
+ON s.[object_id] = i.[object_id]
+WHERE OBJECTPROPERTY(s.[object_id],'IsUserTable') = 1
+AND i.index_id = s.index_id
+AND s.database_id = DB_ID()
+ORDER BY user_seeks + user_scans + user_lookups DESC OPTION (RECOMPILE); -- Order by reads
+
+
+-- Show which indexes in the current database are most active for Reads
+
+
+--- Index Read/Write stats (all tables in current DB) ordered by Writes  (Overall Index Usage - Writes)
+SELECT OBJECT_NAME(s.[object_id]) AS [ObjectName], i.name AS [IndexName], i.index_id,
+	   s.user_updates AS [Writes], s.user_seeks + s.user_scans + s.user_lookups AS [Reads], 
+	   i.type_desc AS [IndexType], i.fill_factor AS [FillFactor], i.has_filter, i.filter_definition,
+	   s.last_system_update, s.last_user_update
+FROM sys.dm_db_index_usage_stats AS s WITH (NOLOCK)
+INNER JOIN sys.indexes AS i WITH (NOLOCK)
+ON s.[object_id] = i.[object_id]
+WHERE OBJECTPROPERTY(s.[object_id],'IsUserTable') = 1
+AND i.index_id = s.index_id
+AND s.database_id = DB_ID()
+ORDER BY s.user_updates DESC OPTION (RECOMPILE);						 -- Order by writes
+
+-- Show which indexes in the current database are most active for Writes
