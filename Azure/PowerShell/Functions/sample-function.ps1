379a967269591c9f69792ac2939b0f640368ee9f
Aderbal Farias <aderbalfarias@hotmail.com>
2019-03-05 21:04:57 +0000

    Adjusting script to create az functions

--- Azure/PowerShell/Functions/sample-function.ps1
+++ Azure/PowerShell/Functions/sample-function.ps1
@@ -1,4 +1,8 @@
-# Creating a function using powershell 
+# Note: Your machine needs to have Azure Function Core Tools running 
+#       in order to create azure function from command line
+npm install -g azure-functions-core-tools
+
+# Creating a function using powershell
 func
 func init # Select a worker runtime and language 
 func start # Start a function
