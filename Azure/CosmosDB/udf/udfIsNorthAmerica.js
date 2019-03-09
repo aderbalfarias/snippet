31f0e53b4cb61d9335d4d4f1eb36960a9c04f65f
Aderbal Farias <aderbalfarias@hotmail.com>
2019-03-09 23:54:10 +0000

    Adding cosmosdb udf

---
+++ Azure/CosmosDB/udf/udfIsNorthAmerica.js
@@ -0,0 +1,8 @@
+﻿function udfIsNorthAmerica(countryRegionName) {
+	var isNorthAmerica =
+		(countryRegionName == 'United States') ||
+		(countryRegionName == 'Canada') ||
+		(countryRegionName == 'Mexico');
+
+	return isNorthAmerica;
+}
