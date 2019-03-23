81f152b9da6e8e1ce8c7507d68c784ea99947247
Aderbal Farias <aderbalfarias@hotmail.com>
2019-03-23 03:23:07 +0000

    Adjusting files

--- Azure/StorageAccounts/Using Shared Access Signatures and Stored Access Policies.sh
+++ Azure/StorageAccounts/Using Shared Access Signatures and Stored Access Policies.sh
@@ -1,33 +1,29 @@
-show generating an account SAS in the portal
+#show generating an account SAS in the portal
+#show generating a container SAP in portal no container level SAS
+#show generating a blob level SAS can't reference a SAP
 
-show generating a container SAP in portal
-- no container level SAS
-
-show generating a blob level SAS
-- can't reference a SAP
-
-Create a Stored Access Policy in Azure CLI
+#Create a Stored Access Policy in Azure CLI
 az storage container policy create --container-name marketingdocs --name writerpolicy01 --account-name exampleorgstgacct01 --permissions lw --expiry 2018-07-25T00:00Z --account-key LmVB0NmKRksq+IEsLDe0dEQjaqYwbPrPX2zUEPqa28Gso3BuP6Qr+rO/8DhQORJ5wfIev3Sf4XDdJ4bjlDadfw==
 
-If you want to delete, just change "create" to "delete"
+#If you want to delete, just change "create" to "delete"
 az storage container policy delete --container-name marketingdocs --name writerpolicy01 --account-name exampleorgstgacct01 --account-key LmVB0NmKRksq+IEsLDe0dEQjaqYwbPrPX2zUEPqa28Gso3BuP6Qr+rO/8DhQORJ5wfIev3Sf4XDdJ4bjlDadfw==
 
-List the Stored Access Policies on a Container
+#List the Stored Access Policies on a Container
 az storage container policy list --container-name marketingdocs --account-name exampleorgstgacct01 --account-key LmVB0NmKRksq+IEsLDe0dEQjaqYwbPrPX2zUEPqa28Gso3BuP6Qr+rO/8DhQORJ5wfIev3Sf4XDdJ4bjlDadfw==
 
-Generate a SAS for a specific Blob, referencing a Stored Access Policy
+#Generate a SAS for a specific Blob, referencing a Stored Access Policy
 az storage blob generate-sas --container-name marketingdocs --name training.txt --account-name exampleorgstgacct01 --account-key LmVB0NmKRksq+IEsLDe0dEQjaqYwbPrPX2zUEPqa28Gso3BuP6Qr+rO/8DhQORJ5wfIev3Sf4XDdJ4bjlDadfw== --policy-name readerpolicy --https-only
 
-"spr=https&sv=2017-07-29&si=readerpolicy&sr=b&sig=9gwY6NcJHD7t/C4czt898gmjjAnpxUDGitdNPAXtwY4%3D"
+#"spr=https&sv=2017-07-29&si=readerpolicy&sr=b&sig=9gwY6NcJHD7t/C4czt898gmjjAnpxUDGitdNPAXtwY4%3D"
 
-Use the SAS to show Blob properties
+#Use the SAS to show Blob properties
 az storage blob show --container-name marketingdocs --name training.txt --account-name exampleorgstgacct01 --sas-token "spr=https&sv=2017-07-29&si=readerpolicy&sr=b&sig=9gwY6NcJHD7t/C4czt898gmjjAnpxUDGitdNPAXtwY4%3D"
 
-Get the URL of the Blob, using the SAS
+#Get the URL of the Blob, using the SAS
 az storage blob url --container-name marketingdocs --name training.txt --account-name exampleorgstgacct01 --sas-token "spr=https&sv=2017-07-29&si=readerpolicy&sr=b&sig=9gwY6NcJHD7t/C4czt898gmjjAnpxUDGitdNPAXtwY4%3D"
 
-"https://exampleorgstgacct01.blob.core.windows.net/marketingdocs/training.txt"
+#"https://exampleorgstgacct01.blob.core.windows.net/marketingdocs/training.txt"
 
-try URL in a browser, without SAS
-try URL in a browser, with SAS appended
+#try URL in a browser, without SAS
+#try URL in a browser, with SAS appended
 
--- Azure/StorageAccounts/Using and Regenerating Storage Account Keys.sh
+++ Azure/StorageAccounts/Using and Regenerating Storage Account Keys.sh
@@ -2,15 +2,15 @@ az login -u xxx@exampleorg.com -p Saturn2005$
 
 az storage blob upload --file "C:\Upload\testfile1.txt" --container-name marketingdocs --name training.txt --account-name exampleorgstgacct01
 
-- storage account not found if user doesn't have permissions
+#storage account not found if user doesn't have permissions
 
 az storage blob upload --file "C:\Upload\testfile1.txt" --container-name marketingdocs --name training.txt --account-name exampleorgstgacct01 --account-key myAccountKey
 
-- successful when using account key (from portal)
+#successful when using account key (from portal)
 
 az storage account list --resource-group exampleorgrg01
 
-- the client does not have authorization to perform action read over scope...
+#the client does not have authorization to perform action read over scope...
 
 az storage account keys list --account-name exampleorgstgacct01 --resource-group exampleorgrg01
 
