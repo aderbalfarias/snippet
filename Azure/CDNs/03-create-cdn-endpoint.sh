187c2b4d8f5fed4f8d12bedec063b52d341884a6
Aderbal Farias <aderbalfarias@hotmail.com>
2019-04-16 23:41:28 +0100

    Location updated

--- Azure/CDNs/03-create-cdn-endpoint.sh
+++ Azure/CDNs/03-create-cdn-endpoint.sh
@@ -3,6 +3,6 @@ az cdn endpoint create \
   --name psazurestoragecdn \
   --profile-name psazurestoragecdn \
   --resource-group pluralsight-azure-storage-cdn \
-  --location 'westeurope' \
+  --location 'northeurope' \
   --origin 'psazurestoragecdn.blob.core.windows.net' \
   --no-http | jq
