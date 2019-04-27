4d7ec27cc4187f96d5b5b804d354148fdc5119d8
Aderbal Farias <aderbalfarias@hotmail.com>
2019-04-27 14:48:44 +0100

    Adjusting azure script

--- Azure/StorageAccounts/Storage Account List.sh
+++ Azure/StorageAccounts/Storage Account List.sh
@@ -5,15 +5,15 @@ az account list-locations --query [*].[displayName,name] --out table
 
 az storage account check-name --name stgacctnm2
 
-az storage account create --name stgacctnm2 --resource-group stgacctnmRG --location eastus --sku Standard_LRS --kind StorageV2 
-az storage account show -g stgacctnmRG -n stgacctnm2
+az storage account create --name stgacctnm2 --resource-group rg-sa --location northeurope --sku Standard_LRS --kind StorageV2 
+az storage account show -g rg-sa -n stgacctnm2
 
-az storage account list -g stgacctnmRG
+az storage account list -g rg-sa
 
-az storage account list -g stgacctnmRG --query [*].{Name:name,Location:primaryLocation,Sku:sku.name,Kind:kind} --out table
+az storage account list -g rg-sa --query [*].{Name:name,Location:primaryLocation,Sku:sku.name,Kind:kind} --out table
 
-az storage account update --name stracctnm2 --resource-group stgacctnmRG --tags "environment=dev"
+az storage account update --name stracctnm2 --resource-group rg-sa --tags "environment=dev"
 
-az storage account list -g stgacctnmRG --query [*].{Name:name,Location:primaryLocation,Sku:sku.name,Kind:kind,Tags:tags.environment} --out table
+az storage account list -g rg-sa --query [*].{Name:name,Location:primaryLocation,Sku:sku.name,Kind:kind,Tags:tags.environment} --out table
 
 az resource list --tag environment=dev
