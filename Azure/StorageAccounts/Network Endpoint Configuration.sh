11e76e648230e6a7a9eb39c05eb185e9f0cb38b9
Aderbal Farias <aderbalfarias@hotmail.com>
2019-04-27 14:45:53 +0100

    Adjusting azure script

--- Azure/StorageAccounts/Network Endpoint Configuration.sh
+++ Azure/StorageAccounts/Network Endpoint Configuration.sh
@@ -1,11 +1,11 @@
-az storage account network-rule list --resource-group "exorgrg01" --account-name "exampleorgstgacct02" --query virtualNetworkRules
+az storage account network-rule list --resource-group "rg-sa" --account-name "exampleorgstgacct02" --query virtualNetworkRules
 
-az network vnet subnet update --resource-group "exorgrg01" --vnet-name "myvnet" --name "mysubnet" --service-endpoints "Microsoft.Storage"
+az network vnet subnet update --resource-group "rg-sa" --vnet-name "myvnet" --name "mysubnet" --service-endpoints "Microsoft.Storage"
 
-subnetid=$(az network vnet subnet show --resource-group "exorgrg01" --vnet-name "exorgvnet01" --name "subnet01" --query id --output tsv)
-az storage account network-rule add --resource-group "exorgrg01" --account-name "exampleorgstgacct02" --subnet $subnetid
+subnetid=$(az network vnet subnet show --resource-group "rg-sa" --vnet-name "exorgvnet01" --name "subnet01" --query id --output tsv)
+az storage account network-rule add --resource-group "rg-sa" --account-name "exampleorgstgacct02" --subnet $subnetid
 
-subnetid=$(az network vnet subnet show --resource-group "exorgrg01" --vnet-name "exorgvnet01" --name "subnet01" --query id --output tsv)
-az storage account network-rule remove --resource-group "exorgrg01" --account-name "exampleorgstgacct02" --subnet $subnetid
+subnetid=$(az network vnet subnet show --resource-group "rg-sa" --vnet-name "exorgvnet01" --name "subnet01" --query id --output tsv)
+az storage account network-rule remove --resource-group "rg-sa" --account-name "exampleorgstgacct02" --subnet $subnetid
 
 az storage account update --name "mystorageaccount" --resource-group "myresourcegroup" --default-action Allow
