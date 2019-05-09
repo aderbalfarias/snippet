e2d1f46465480afd3c63969892c011196b194fc1
Aderbal Farias <aderbalfarias@hotmail.com>
2019-05-10 00:21:38 +0100

    Fixing scripts

--- Azure/Security/grant-key-vault.ps1
+++ Azure/Security/grant-key-vault.ps1
@@ -3,7 +3,7 @@ Login-AzureRmAccount
 
 $subscriptionName = 'Pay-As-You-Go'
 $userPrincipalName = 'aderbal@aderbal.com'
-$applicationId = '386424df-c14a-4436-b872-f186ea2ddc98'
+$applicationId = 'mySubscriptionId'
 $resourceGroupName = 'rm-security'
 $location = 'northeurope'
 $vaultName = 'AwysEncKV'
