dba889f2e00da8400f755a6ebf395919a04751a7
Aderbal Farias <aderbalfarias@hotmail.com>
2019-05-29 23:44:59 +0100

    Insertion sort algorithm updated

--- Algorithms/InsertionSort.cs
+++ Algorithms/InsertionSort.cs
@@ -1,32 +1,4 @@
-// Insertion sort is stable, in-place sorting algorithm that builds the final sorted array 
-// one item at a time. It is not very best in terms of performance but it is more efficient 
-// in practice than most other simple O(n2) algorithms such as selection sort or bubble sort. 
-// Insertion sort is also used is used in Hybrid sort which combines different sorting 
-// algorithms to improve performance.
-// It is also a well known online algorithm as it can sort a list as it receives it. 
-// In all other algorithms we need all elements to be provided to the sorting algorithm 
-// before applying it. But an insertion sort allows we to start with partial set of elements, 
-// sorts it (called as partially sorted set), and if we want, we can insert more elements 
-// (these are the new set of elements that were not in memory when the sorting started) and 
-// sorts these elements too. In real world, data to be sorted is usually not static, rather dynamic. 
-// If even one additional element is inserted during the sort process, other algorithms don't respond 
-// easily. But only this algorithm is not interrupted and can respond well with the additional element.
-
-// How it works?
-// The idea is to divide the array into two subsets – sorted subset and unsorted subset. 
-// Initally sorted subset consists of only one first element at index 0. Then for each iteration, 
-// insertion sort removes next element from the unsorted subset, finds the location it belongs 
-// within the sorted subset, and inserts it there. It repeats until no input elements remain. 
-
-// You can see a image on Images/InsertionSort.png
-// i = 1    [  3  8  5  4  1  9  -2 ]
-// i = 2    [  3  8  5  4  1  9  -2 ]           
-// i = 3    [  3  5  8  4  1  9  -2 ]              
-// i = 4    [  3  4  5  8  1  9  -2 ]
-// i = 5    [  1  3  4  5  8  9  -2 ]
-// i = 6    [  1  3  4  5  8  9  -2 ]
-//          [ -2  1  3  4  5  8   9 ]
- 
+//Note: Comments on the bottom of this file
 public class InsertionSort
 {
 	// Function to perform insertion sort on array[]
@@ -92,3 +64,32 @@ public class InsertionSort
 	}
 }
 
+// Insertion sort is stable, in-place sorting algorithm that builds the final sorted array 
+// one item at a time. It is not very best in terms of performance but it is more efficient 
+// in practice than most other simple O(n2) algorithms such as selection sort or bubble sort. 
+// Insertion sort is also used is used in Hybrid sort which combines different sorting 
+// algorithms to improve performance.
+// It is also a well known online algorithm as it can sort a list as it receives it. 
+// In all other algorithms we need all elements to be provided to the sorting algorithm 
+// before applying it. But an insertion sort allows we to start with partial set of elements, 
+// sorts it (called as partially sorted set), and if we want, we can insert more elements 
+// (these are the new set of elements that were not in memory when the sorting started) and 
+// sorts these elements too. In real world, data to be sorted is usually not static, rather dynamic. 
+// If even one additional element is inserted during the sort process, other algorithms don't respond 
+// easily. But only this algorithm is not interrupted and can respond well with the additional element.
+
+// How it works?
+// The idea is to divide the array into two subsets – sorted subset and unsorted subset. 
+// Initally sorted subset consists of only one first element at index 0. Then for each iteration, 
+// insertion sort removes next element from the unsorted subset, finds the location it belongs 
+// within the sorted subset, and inserts it there. It repeats until no input elements remain. 
+
+// You can see a image on Images/InsertionSort.png
+// i = 1    [  3  8  5  4  1  9  -2 ]
+// i = 2    [  3  8  5  4  1  9  -2 ]           
+// i = 3    [  3  5  8  4  1  9  -2 ]              
+// i = 4    [  3  4  5  8  1  9  -2 ]
+// i = 5    [  1  3  4  5  8  9  -2 ]
+// i = 6    [  1  3  4  5  8  9  -2 ]
+//          [ -2  1  3  4  5  8   9 ] Final output
+ 
