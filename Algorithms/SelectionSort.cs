1dca48ce6971e5614c06a9d159e28edb7316c2a8
Aderbal Farias <aderbalfarias@hotmail.com>
2019-05-29 23:42:05 +0100

    Selection sort algorithm updated

--- Algorithms/SelectionSort.cs
+++ Algorithms/SelectionSort.cs
@@ -26,14 +26,37 @@ public class SelectionSort
 			Swap(array, min, i);
 		}
 	}
+	
+		// Recursive function to perform selection sort on sub-array arr[i..n-1]
+	public static void SelectionSortV2(int[] array, int i, int n)
+	{
+		// find the minimum element in the unsorted sub-array[i..n-1]
+		// and swap it with array[i]
+		int min = i;
+		for (int j = i + 1; j < n; j++)
+			// if array[j] element is less, then it is the new minimum
+			if (array[j] < array[min])
+				min = j; // update index of min element
+
+		// swap the minimum element in sub-array[i..n-1] with array[i]
+		Swap(array, min, i);
+
+		if (i + 1 < n)
+			SelectionSortV2(array, i + 1, n);
+	}
 
 	public static void Main()
 	{
 		int[] array = { 3, 5, 8, 4, 1, 9, -2 };
 
 		SelectionSortV1(array);
-
 		// print the sorted array
-        Console.WriteLine(string.Join(",", array));
+        Console.WriteLine($"v1: [ { string.Join(", ", array) } ]");
+		
+		int[] arrayRecursive = { 3, 5, 8, 4, 1, 9, -2 };
+		
+		SelectionSortV2(arrayRecursive, 1, arrayRecursive.Length - 1);
+		// print the sorted array
+        Console.WriteLine($"v2: [ { string.Join(", ", array) } ]");
 	}
 }
