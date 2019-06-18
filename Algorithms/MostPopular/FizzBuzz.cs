47b19942d7a38c395c5baed309a092b85cf4ad5c
Aderbal Farias <aderbalfarias@hotmail.com>
2019-06-18 22:12:49 +0100

    Adding algorithm

---
+++ Algorithms/MostPopular/FizzBuzz.cs
@@ -0,0 +1,20 @@
+public class FizzBuzz
+{
+	public void Main()
+	{
+		for (int i = 1; i <= 100; i++)
+	    {
+	        var fizz = i % 3 == 0;
+	        var buzz = i % 5 == 0;
+	
+	        if (fizz && buzz)
+	            Console.WriteLine("FizzBuzz");
+	        else if (fizz)
+	            Console.WriteLine("Fizz");
+	        else if (buzz)
+	            Console.WriteLine("Buzz");
+	        else
+	            Console.WriteLine(i);
+	    }
+	}
+}
