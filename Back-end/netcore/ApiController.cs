8cb4028234d6af0ec3bc1a722bda98a8d4b8b36e
Aderbal Farias <aderbalfarias@hotmail.com>
2019-08-20 16:53:47 +0100

    Example of a controller in net core with Api

--- Back-end/netcore/ApiController.cs
+++ Back-end/netcore/ApiController.cs
@@ -1,1 +1,33 @@
-// Example of a controller in netcore with web api
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ProjectNameApi.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/test")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly ITestService _testService;
+        private readonly ILogger _logger;
+
+        public TestController(ITestService testService, ILogger<TestController> logger)
+        {
+            _testService = testService;
+            _logger = logger;
+        }
+
+        // GET api/test/getall
+        [HttpGet]
+        [EnableCors]
+        [Route("getall")]
+        public IActionResult GetAll() => Ok(_testService.GetAll());
+
+        // GET api/test/getbyid/{id}
+        [HttpGet]
+        [EnableCors]
+        [Route("getbyid/{id:int:min(1)}")]
+        public IActionResult GetById(int id) => Ok(_testService.GetById(id));
+    }
+}
