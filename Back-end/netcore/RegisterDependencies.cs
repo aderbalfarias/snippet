8cada45b8a99f66553379df8a891ec812f3278a5
Aderbal Farias <aderbalfarias@hotmail.com>
2019-08-24 18:03:14 +0100

    Adjusting content

--- Back-end/netcore/RegisterDependencies.cs
+++ Back-end/netcore/RegisterDependencies.cs
@@ -1,26 +1,29 @@
 namespace ProjectNameApi.IoC
 {
-    public static IServiceCollection Services(this IServiceCollection services)
-    {
-        services.AddScoped<ITestService, TestService>();
+	public static class RegisterDependencies
+	{
+		public static IServiceCollection Services(this IServiceCollection services)
+		{
+			services.AddScoped<ITestService, TestService>();
 
-        return services;
-    }
+			return services;
+		}
 
-    public static IServiceCollection Repositories(this IServiceCollection services)
-    {
-        services.AddScoped<ITestRepository, TestRepository>();
+		public static IServiceCollection Repositories(this IServiceCollection services)
+		{
+			services.AddScoped<ITestRepository, TestRepository>();
 
-        return services;
-    }
+			return services;
+		}
 
-    public static IServiceCollection Databases(this IServiceCollection services, string connectionString)
-    {
-        // Add configuration for DbContext
-        // Use connection string from appsettings.json file
-        services.AddDbContext<Context>(options => options.UseSqlServer(connectionString));
-        services.AddScoped<ILogger, Logger<Context>>();
+		public static IServiceCollection Databases(this IServiceCollection services, string connectionString)
+		{
+			// Add configuration for DbContext
+			// Use connection string from appsettings.json file
+			services.AddDbContext<Context>(options => options.UseSqlServer(connectionString));
+			services.AddScoped<ILogger, Logger<Context>>();
 
-        return services;
-    }
+			return services;
+		}
+	}
 }
