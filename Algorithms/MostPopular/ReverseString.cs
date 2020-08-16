0d2ada5f14943af33db769b065bded158180742a
Aderbal Farias <aderbalfarias@hotmail.com>
2020-08-16 04:26:40 +0100

    Updating algorithm

--- Algorithms/MostPopular/ReverseString.cs
+++ Algorithms/MostPopular/ReverseString.cs
@@ -2,25 +2,27 @@ class Program
 {  
     static void Main(string[] args)  
     {  
-        string str = "", reverse = "";  
-        int Length = 0;  
+        string str = string.Empty;   
 		
         Console.WriteLine("Enter a Word");  
 		
-        //Getting String(word) from Console  
+        // Getting String(word) from Console  
         str = Console.ReadLine();  
 		
-        //Calculate length of string str  
-        Length = str.Length - 1;  
-		
-        while(Length >= 0)  
-        {  
-            reverse = reverse + str[Length];  
-            Length--;  
-        }  
-		
-        //Displaying the reverse word  
-        Console.WriteLine("Reverse word is {0}",reverse);  
+        // Displaying the reverse word  
+        Console.WriteLine($"Reverse word is {ReverseWord(str)}");  
         Console.ReadLine();  
     }  
+	
+	public static string ReverseWord(string str)
+	{
+		string reverse = string.Empty;   
+		
+		// Calculate length of string str  
+		int length = str.Length - 1;  
+		for(int i = length; i >= 0; i--)
+			reverse = reverse + str[i];  
+			
+		return reverse;
+	}
 }  
