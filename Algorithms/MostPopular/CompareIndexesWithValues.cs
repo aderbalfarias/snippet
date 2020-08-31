3454bf9feebb85cfc48e1537cb5cb88df37e40d0
Aderbal Farias <aderbalfarias@hotmail.com>
2020-08-31 23:39:19 +0100

    Adjusting algorithm output

--- Algorithms/MostPopular/CompareIndexesWithValues.cs
+++ Algorithms/MostPopular/CompareIndexesWithValues.cs
@@ -4,9 +4,9 @@ public static class CompareIndexesWithValues
 	static void Main(string[] args)
     {
         Console.WriteLine("Tests Algorithm");
-        Console.WriteLine(Calc(new int[] { 2, 1, 3, 5, 4 })); //Should be 3
-        Console.WriteLine(Calc(new int[] { 2, 3, 4, 1, 5 })); //Should be 2
-        Console.WriteLine(Calc(new int[] { 1, 3, 4, 2, 5 })); //Should be 3
+        Console.WriteLine($"Should return 3: { Calc(new int[] { 2, 1, 3, 5, 4 }) }"); //Should be 3
+        Console.WriteLine($"Should return 2: { Calc(new int[] { 2, 3, 4, 1, 5 }) }"); //Should be 2
+        Console.WriteLine($"Should return 3: { Calc(new int[] { 1, 3, 4, 2, 5 }) }"); //Should be 3
     }
 		
     public static int Calc(int[] a)
