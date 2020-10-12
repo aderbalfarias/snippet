cd2bb2779fb204c996aa51f0c0f2b7c927e962e0
Aderbal Farias <aderbalfarias@hotmail.com>
2020-10-12 17:28:54 +0100

    Update git-commands.sh

--- Commands/git-commands.sh
+++ Commands/git-commands.sh
@@ -26,6 +26,7 @@ git add -A #> Add all new and changed files to the staging area
 git rm -r [file-name] #> Remove a file from staging area
 git stash save "message" #> It creates a stash that allows we pull code and then merge the code without commit it, like config files
 git stash apply stash@{[stash number]} #> Get back the files stashed
+git stash list #> Get list of stashes
 git stash pop stash@{[stash number]} #> Return your repository to status before the stash (you will lose changes)
 git stash drop stash@{[stash number]} #> Delete stash
 git stash apply $stash_rash #> Get back the files from a deleted stash
