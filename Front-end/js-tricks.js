c6fb284d736c1a1918e85388a99cbe4bb56c7d1c
Aderbal Farias <aderbalfarias@hotmail.com>
2022-07-09 04:15:06 +0100

    Create js-tricks.js

---
+++ Front-end/js-tricks.js
@@ -0,0 +1,6 @@
+// rotate the page
+(function(){
+    document.documentElement.style.transitionDuration = '60s';
+    document.documentElement.style.transitionTimingFunction = 'easy-in';
+    document.documentElement.style.transform = 'rotate(360deg)';
+}());
